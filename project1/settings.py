5ea5568d9c51078edef9922877f919aeb1c08b53
pranavtheme <djangorestpraispranav@gmail.com>
2021-04-20 19:31:02 +0530

    third

--- project1/settings.py
+++ project1/settings.py
@@ -114,8 +114,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
+MEDIA_URL = '/media/'
